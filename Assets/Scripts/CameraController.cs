874d1f8344843bdc2c33b54d4552bf67ad58becf
Maximilian Stiller <maximilian.stiller1992@gmail.com>
2023-05-12 19:18:19 +0200

    feat: add first person camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,10 +6,11 @@ public class CameraController : MonoBehaviour
 {
     //Public Variables
     public GameObject player;
+    public bool isMainCamera = true;
 
     //Private Variables
-    private Vector3 offset = new(0, 5, -6); 
-
+    private Vector3 mainOffset = new(0, 5, -6);
+    private Vector3 hoodOffset = new(0, 2.1f, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,9 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+
+        transform.position = player.transform.position + (isMainCamera ? mainOffset : hoodOffset);
+
+        if (!isMainCamera) transform.rotation = player.transform.rotation;
     }
 }
